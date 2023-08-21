39f79443faed5a85043b8f452678e027e0851985
codyGirl-27 <142378947+codyGirl-27@users.noreply.github.com>
2023-08-21 17:58:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  
+  background("blue")
+  
+  fill("red")
+  
+  rect(10,220,10,80);
+  
+  rect(380,220,10,80);
+  
+}
+
+//1 - left and right - x axis  
+
+// 2 - up and down - y axis 
+
+//3 - width 
+
+//4 -height 
